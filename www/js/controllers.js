9bdff7aefb6e45d2249289a7846069eb9760612e
log4john <log4john@yahoo.com>
2016-06-09 22:12:06 +0530

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,21 @@
+angular.module('starter.controllers', [])
+
+.controller('LoginCtrl', function($scope, $http) {
+    $scope.data = {};
+
+    $scope.login = function() {
+		
+		$http.post("http://localhost:8124")
+			.success(function(data){
+				
+					$scope.data.message = data.message;
+				
+			})
+			.error(function (error){
+				alert(error);
+				
+			});       
+    }
+});
+
+
